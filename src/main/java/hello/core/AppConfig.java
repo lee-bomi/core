e08da4a76165ef52c1a74f0f7859dd3e2ad73693
leebomi <bomvll@naver.com>
2021-07-30 22:14:23 +0900

    Apply object-oriented principle

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,6 @@
+package hello.core;
+
+public class AppConfig {  //애플리케이션 전체를 설정하고 구성한다(공연기획자)
+
+
+}
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override //MemberRepository에서 회원정보가져와서, DiscountPolicy에서 할인금액 가져와서 주문을 만든다
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
